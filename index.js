fd7c0ba3b935a84177f402e9a87804f61137de17
Karissa McKelvey <krmckelv@gmail.com>
2019-05-01 01:44:27 +0300

    finish up the first go at it

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var debug = require('debug')('kappa-fs')
 var kv = require('kappa-view-kv')
 var memdb = require('memdb')
 var hyperdrive = require('hyperdrive')
+var duplexify = require('duplexify')
+
+function dumbMerge (values) {
+  return values[0]
+}
 
 class KappaDrive {
   constructor (storage, opts) {
@@ -11,10 +16,12 @@ class KappaDrive {
     this.core = kappa(storage)
     this._storage = storage
     this._index = opts.index || memdb()
+    this._resolveFork = opts.resolveFork || dumbMerge
 
     this.kvidx = kv(this._index, function (msg, next) {
       var ops = []
       var msgId = msg.key + '@' + msg.seq
+      // TODO: ew, json?
       try {
         var value = JSON.parse(msg.value)
       } catch (err) {
@@ -27,7 +34,6 @@ class KappaDrive {
         content: value.content,
         links: value.links || []
       })
-      debug('ops', ops)
       next(null, ops)
     })
     this.core.use('kv', this.kvidx)
@@ -50,53 +56,106 @@ class KappaDrive {
     })
   }
 
-  readFile (filename, cb) {
-    if (!this._open) throw new Error('not ready yet, try calling .ready')
+  _whoHasFile (filename, cb) {
     this.core.ready('kv', () => {
       this.core.api.kv.get(filename, (err, values) => {
         if (err && !err.notFound) return cb(err)
-        // get metadata and content feeds for the values here
-        if (!values.length) this.drive.readFile(filename, cb)
-        else {
-          var v = values[0]
-          var value = JSON.parse(v.value)
-          debug('readFile for', value)
-          this._getDrive(value.metadata, value.content, (err, drive) => {
-            if (err) return cb(err)
-            drive.readFile(filename, cb)
-          })
-        }
+        if (!values || !values.length) return cb(null, this.drive)
+        var winner = this._resolveFork(values)
+        // TODO: allow multiple winners
+        var value = JSON.parse(winner.value)
+        this._getDrive(value.metadata, value.content, cb)
       })
     })
   }
 
-  replicate () {
-    return this.core.replicate()
+  createReadStream (filename) {
+    var proxy = duplexify()
+    this._whoHasFile(filename, (err, drive) => {
+      if (err) return proxy.emit('err', err)
+      proxy.setReadable(drive.createReadStream(filename))
+    })
+    return proxy
   }
 
-  writeFile (filename, content, cb) {
+  readFile (filename, opts, cb) {
     if (!this._open) throw new Error('not ready yet, try calling .ready')
+    this._whoHasFile(filename, (err, drive) => {
+      if (err) return cb(err)
+      drive.readFile(filename, opts, cb)
+    })
+  }
+
+  _getLinks (filename, cb) {
     this.core.ready('kv', () => {
       this.core.api.kv.get(filename, (err, values) => {
         if (err && !err.notFound) return cb(err)
         var links = values ? values.map((v) => v.key + '@' + v.seq) : []
-        this.drive.writeFile(filename, content, (err) => {
-          if (err) return cb(err)
-          var res = {
-            filename,
-            metadata: this.drive.metadata.key.toString('hex'),
-            content: this.drive.content.key.toString('hex'),
-            links: links
-          }
-          debug('appending', content, res, content)
-          this.local.append(JSON.stringify(res), cb)
-        })
+        return cb(null, links)
       })
     })
   }
 
+  _finishWrite (filename, links, cb) {
+    var res = {
+      filename,
+      links
+    }
+
+    // TODO: we probably should record the seq of the metadata/content as well
+    // and perform a checkout to that hyperdrive seq on reads
+    res.metadata = this.drive.metadata.key.toString('hex')
+    res.content = this.drive.content.key.toString('hex')
+
+    debug('writing finished', res)
+
+    // TODO: ew JSON stringify is slow... lets use protobuf instead
+    this.local.append(JSON.stringify(res), cb)
+  }
+
+  writeFile (filename, content, cb) {
+    this._getLinks(filename, (err, links) => {
+      if (err) return cb(err)
+      this.drive.writeFile(filename, content, (err) => {
+        if (err) return cb(err)
+        this._finishWrite(filename, links, cb)
+      })
+    })
+  }
+
+  createWriteStream (filename) {
+    var proxy = duplexify()
+    this._getLinks(filename, (err, links) => {
+      if (err) proxy.emit('error', err)
+      // TODO: possible race condition with the indexes not catching up before
+      // the next write... need to call finishWrite afterbefore the writer ends
+      var writer = this.drive.createWriteStream(filename)
+      proxy.setWritable(writer)
+
+      var prefinish = () => {
+        proxy.cork()
+        this._finishWrite(filename, links, (err) => {
+          if (err) return proxy.destroy()
+          proxy.uncork()
+        })
+      }
+
+      proxy.on('close', done)
+      proxy.on('finish', done)
+      proxy.on('prefinish', prefinish)
+
+      function done () {
+        proxy.removeListener('close', done)
+        proxy.removeListener('finish', done)
+        proxy.removeListener('prefinish', prefinish)
+      }
+    })
+
+    return proxy
+  }
+
   open (cb) {
-    this.core.feed('mydata', (err, feed) => {
+    this.core.feed('peerfs', (err, feed) => {
       if (err) cb(err)
       this.local = feed
       this._getDrive('metadata', 'content', (err, drive) => {
@@ -114,13 +173,13 @@ class KappaDrive {
     if (this._open) return cb()
     this.open(cb)
   }
+
+  replicate () {
+    return this.core.replicate()
+  }
 }
 
 module.exports = function (storage, opts) {
   // what do we do with the key??
   return new KappaDrive(storage, opts)
-}
-
-function version (feed, seq) {
-  return feed.key.toString('hex') + '@' + seq
 }
--- test.js
+++ test.js
@@ -1,12 +1,18 @@
 var rimraf = require('rimraf')
 var test = require('tape')
 var pump = require('pump')
-var kappadrive = require('./')
+var tmpdir = require('tmp').dirSync
+var kappafs = require('./')
+var mkdirp = require('mkdirp')
 
-rimraf.sync('./db')
+var tmp = function () {
+  var dir = tmpdir().name
+  mkdirp.sync(dir)
+  return dir
+}
 
-test('test write and read latest value', function (t) {
-  var drive = kappadrive('./db')
+test('basic: write and read latest value', function (t) {
+  var drive = kappafs(tmp())
 
   drive.ready(() => {
     drive.writeFile('/hello.txt', 'world', function (err) {
@@ -20,12 +26,24 @@ test('test write and read latest value', function (t) {
   })
 })
 
-rimraf.sync('./db-1')
-rimraf.sync('./db-2')
+test('basic: writeStream and readStream', function (t) {
+  var drive = kappafs(tmp())
+  drive.ready(() => {
+    var ws = drive.createWriteStream('/hello.txt')
+    ws.end('world')
+    ws.on('finish', () => {
+      var rs = drive.createReadStream('/hello.txt')
+      rs.on('data', (data) => {
+        t.same(data.toString(), 'world')
+        t.end()
+      })
+    })
+  })
+})
 
-test('test default to latest value', function (t) {
-  var drive = kappadrive('./db-1')
-  var drive2 = kappadrive('./db-2')
+test('multiwriter: defaults to latest value', function (t) {
+  var drive = kappafs(tmp())
+  var drive2 = kappafs(tmp())
 
   drive.ready(() => {
     drive.writeFile('/hello.txt', 'world', function (err) {
@@ -47,14 +65,10 @@ test('test default to latest value', function (t) {
   }
 
   function sync () {
-    var stream = drive.replicate()
-    var d = drive2.replicate()
-    pump(d, stream, d, (err) => {
+    replicate(drive, drive2, (err) => {
       t.error(err)
       writeSecond(() => {
-        var stream = drive.replicate()
-        var d = drive2.replicate()
-        pump(d, stream, d, (err) => {
+        replicate(drive, drive2, (err) => {
           t.error(err)
           drive.readFile('/hello.txt', function (err, data) {
             t.error(err)
@@ -67,3 +81,85 @@ test('test default to latest value', function (t) {
     })
   }
 })
+
+test('multiwriter defaults to latest value with streams', function (t) {
+  var drive = kappafs(tmp())
+  var drive2 = kappafs(tmp())
+
+  drive.ready(() => {
+    drive2.ready(() => {
+      var ws = drive.createWriteStream('/hello.txt')
+      ws.on('finish', sync)
+      ws.end('mundo')
+    })
+  })
+
+  function writeSecond (cb) {
+    var ws = drive2.createWriteStream('/hello.txt')
+    ws.on('finish', cb)
+    ws.on('error', t.error)
+    ws.end('verden')
+  }
+
+  function sync () {
+    replicate(drive, drive2, (err) => {
+      t.error(err)
+      writeSecond(() => {
+        replicate(drive, drive2, (err) => {
+          t.error(err)
+          var rs = drive.createReadStream('/hello.txt')
+          rs.on('data', (data) => {
+            t.same(data.toString(), 'verden')
+            t.end()
+          })
+        })
+      })
+    })
+  }
+})
+
+test('fork', function (t) {
+  var drive = kappafs(tmp())
+  var drive2 = kappafs(tmp())
+
+  drive.ready(() => {
+    drive2.ready(() => {
+      var ws = drive.createWriteStream('/hello.txt')
+      ws.end('world')
+      ws.on('finish', () => replicate(drive, drive2, writeFork))
+    })
+  })
+
+  function writeFork () {
+    var pending = 2
+    var done = () => !--pending && replicate(drive, drive2, checkFork)
+    var ws = drive.createWriteStream('/hello.txt')
+    ws.on('finish', done)
+    ws.end('mundo')
+    var ws2 = drive2.createWriteStream('/hello.txt')
+    ws2.on('finish', done)
+    ws2.end('verden')
+  }
+
+  function checkFork () {
+    var ws = drive.createWriteStream('/hello.txt')
+    ws.end('whateverr')
+    ws.on('finish', () => {
+      drive2.readFile('/hello.txt', 'utf-8', function (err, _drive2) {
+        t.error(err)
+        t.same(_drive2, 'verden')
+        drive.readFile('/hello.txt', 'utf-8', function (err, _drive1) {
+          t.error(err)
+          t.same(_drive1, 'whateverr')
+          t.end()
+        })
+      })
+    })
+  }
+})
+
+function replicate (drive1, drive2, cb) {
+  var s = drive1.replicate()
+  var d = drive2.replicate()
+  pump(d, s, d, cb)
+}
